602ff46ade7674599fbff06e5168e8537d470b09
Mablen <marcos.broseta@gmail.com>
2020-12-15 13:09:25 +0100

    última modificación

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Clases de entrada y salida
+
+<p>A la hora de trabajar con flujos de datos de entrada y salida, Java
+ofrece la librería java.io</p>
--- src/EntradaSalida/Test01.java
+++ src/EntradaSalida/Test01.java
@@ -8,16 +8,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Test01{
-	public static void main(String[] args) {
-		System.out.println("Escribe una frase: ");
+	public static void main(String[] args) throws IOException{
+		System.out.println("Introduce un texto por teclado: ");
 		// Entrada
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         //Se podría escribir en una sola línea, de este modo ahorraríamos código.
         //BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-        String frase = br.readLine();
+        String texto = br.readLine();
         //Salida
-        System.out.println("Esta es la frase escrita: " + frase);
+        System.out.println("Esta es el texto introducido: " + texto);
 		
 	}
 }
